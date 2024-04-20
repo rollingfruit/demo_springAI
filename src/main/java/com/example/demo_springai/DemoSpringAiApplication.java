3f60e102c26d84aeed13cfa33c4bcedd905a1cad
xiaoxiao <839919877@qq.com>
2024-04-20 12:42:48 +0900

    官方demo成功，200 ok

---
+++ src/main/java/com/example/demo_springai/DemoSpringAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_springai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demo_springai/DemoSpringAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_springai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
